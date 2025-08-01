22bbaaaed3236cdc3e94c7a56cebbd4fb168e707
vigonotion <vigonotion@users.noreply.github.com>
2025-08-01 22:58:08 +0200

    crazy chain adjustments

--- scripts/paternoster_chain.gd
+++ scripts/paternoster_chain.gd
@@ -6,16 +6,33 @@ extends Path3D
 		count = c
 		_init()
 
+@export_tool_button("Re-init chain path")
+var button = _init
+
 const paternoster_height = 3
+@export var curve_height = 6.0:
+	set(c):
+		curve_height = c
+		_init()
 
 func _init() -> void:
+	print("ch", curve_height)
 	curve = Curve3D.new() 
 	var halfcount = count / 2.0
 	var y_offset = -count / 4 * paternoster_height
 	
 	for i in range(count):
-		if i < halfcount:
+
+		if i == 0:
+			curve.add_point(Vector3(0, y_offset + i * paternoster_height, 0), Vector3.DOWN * curve_height)
+		if i == halfcount - 1:
+			curve.add_point(Vector3(0, y_offset + i * paternoster_height, 0), Vector3.ZERO, Vector3.UP * curve_height)
+		elif i < halfcount:
 			curve.add_point(Vector3(0, y_offset + i * paternoster_height, 0))
+		elif i == halfcount:
+			curve.add_point(Vector3(3, y_offset + (halfcount * paternoster_height) - (i - halfcount + 1)* paternoster_height, 0), Vector3.UP * curve_height)
+		elif i == count - 1:
+			curve.add_point(Vector3(3, y_offset + (halfcount * paternoster_height) - (i - halfcount + 1)* paternoster_height, 0), Vector3.ZERO, Vector3.DOWN * curve_height)
 		else:
 			curve.add_point(Vector3(3, y_offset + (halfcount * paternoster_height) - (i - halfcount + 1)* paternoster_height, 0))
 			
@@ -28,9 +45,11 @@ func _ready() -> void:
 		
 	var scene = load("res://scenes/chainlink.tscn")
 	
-	for i in range(count):
+	var paternoster_count = count + 4
+
+	for i in range(paternoster_count):
 		var instance = scene.instantiate()
-		instance.offset = 1.0/count + i/float(count)
+		instance.offset = 1.0/paternoster_count + i/float(paternoster_count)
 		add_child(instance)
 		print("Add child")
 	

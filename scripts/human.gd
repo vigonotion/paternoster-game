7e4b59ba27c3cc524e5b24d4c2f264d8dda1f355
vigonotion <1506970+vigonotion@users.noreply.github.com>
2025-08-01 19:39:23 +0200

    queue system, only control first in line

--- scripts/human.gd
+++ scripts/human.gd
@@ -8,11 +8,27 @@ extends RigidBody3D
 
 @onready var bubble: Node3D = $Bubble
 
-func _process(delta: float) -> void:
-	bubble.visible = is_waiting_enter_up || is_waiting_leave_up || is_waiting_enter_down || is_waiting_leave_down
+@onready var raycast: RayCast3D = $Raycast
+
+
+func _process(delta: float) -> void:	
+	
 	
 	if (is_waiting_enter_up and Input.is_action_just_pressed("enter_up")) or (is_waiting_enter_down and Input.is_action_just_pressed("enter_down")):
 		apply_impulse(Vector3(0, 100, -300), Vector3(0, 1, 0))
 	elif (is_waiting_leave_down and Input.is_action_just_pressed("leave_down")) or (is_waiting_leave_up and Input.is_action_just_pressed("leave_up")):
 		apply_impulse(Vector3(0, 100, 300), Vector3(0, 1, 0))
 	
+
+var i = 0
+func _physics_process(delta: float) -> void:
+	i += 1
+
+	if raycast.is_colliding():
+		return
+	
+	if is_waiting_enter_up or is_waiting_enter_down or is_waiting_leave_up or is_waiting_leave_down:
+		return	
+	
+	if i > 100 and i % 25 == 0:
+		apply_impulse(Vector3(0, 100, -50), Vector3(0, 1, 0))
